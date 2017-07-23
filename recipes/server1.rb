280b5939260ed66e46eb3bfb2d635648ca6a72f3
vagrant <vagrant@localhost.localdomain>
2017-07-23 18:46:24 +0000

    Version 0.2.1 - Updated index.html with system attributes with node object/ohai

--- recipes/server1.rb
+++ recipes/server1.rb
@@ -5,7 +5,10 @@ end
 
 
 file '/var/www/html/index.html' do
-  content '<h1>THIS IS FIRST APACHE INSTALLATION, WELCOME...!</h1>'
+  content "<h1>THIS IS FIRST APACHE INSTALLATION, WELCOME...!</h1>
+    <h2> HOSTNAME: #{node['hostname']} </h2>
+    <h2> IPADDRESS: #{node['ipaddress']} </h2>
+"
 end
 
 service 'httpd' do
